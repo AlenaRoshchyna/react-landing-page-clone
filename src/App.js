4a65c695c32d4625ab267e8e75bd961ec21c62ee
Alena Roshchyna <girls.code.better@gmail.com>
2023-05-09 16:58:37 +0200

    Set up the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styles from './App.module.scss';
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <div className={styles.head}>
+          <span>kjdfglkjsfdl</span>
+        </div>
+      </header>
+      <main>dfbgdr</main>
+      <footer>rdgbdf</footer>
+    </>
+  );
+}
