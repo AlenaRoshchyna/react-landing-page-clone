5c05b016a14b7f6f05f9e77dd9bf23304fa286c6
Alena Roshchyna <girls.code.better@gmail.com>
2023-05-11 15:09:26 +0200

    Rearrange css structure

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ export default function App() {
     <>
       <header>
         <nav className={styles.navigationBar}>
-          <Logo />
-          <a href="/#">
-            <span>
-              startups
-              <span> to</span> fund
-            </span>
-          </a>
+          <div className={styles.logo}>
+            <Logo />
+            <a href="/#">
+              <span>
+                startups
+                <span>to</span>fund
+              </span>
+            </a>
+          </div>
+
+          <ul className={styles.navList}>
+            <li>
+              <a href="/#">INVEST</a>
+              <a href="/#">Find promising startups</a>
+            </li>
+
+            <li>
+              <a href="/#">FUNDRAISE </a>
+              <a href="/#">Raise money</a>
+            </li>
+          </ul>
+          <div className={styles.sign}>
+            <a href="/#">Sign up</a>
+            <a href="/#">Sign in</a>
+          </div>
         </nav>
       </header>
-      <main></main>
+      <main>
+        <img></img>
+      </main>
       <footer></footer>
     </>
   );
